42072b66a8e63f3618c31dad0651ca44f5511208
erynjones <eryn.jones16@hotmail.com>
2016-02-28 19:27:26 -0500

    Everything

---
+++ js/main.js
@@ -0,0 +1,51 @@
+var letter = prompt('Enter a Letter');
+
+for (i = 0; i < 1; i++) {
+  document.write('b');
+  document.write('<br>');
+}
+
+for (i = 1; i < 2; i++) {
+  document.write('bb');
+  document.write('<br>');
+}
+
+for (i = 2; i < 3; i++) {
+  document.write('bbb');
+  document.write('<br>');
+}
+
+for (i = 3; i < 4; i++) {
+  document.write('bbbb');
+  document.write('<br>');
+}
+
+for (i = 4; i < 5; i++) {
+  document.write('bbbbb');
+  document.write('<br>');
+}
+
+for (i = 5; i < 6; i++) {
+  document.write('bbbbbb');
+  document.write('<br>');
+}
+
+for (i = 6; i < 7; i++) {
+  document.write('bbbbbbb');
+  document.write('<br>');
+}
+
+for (i = 7; i < 8; i++) {
+  document.write('bbbbbbbb');
+  document.write('<br>');
+}
+
+for (i = 8; i < 9; i++) {
+  document.write('bbbbbbbbb');
+  document.write('<br>');
+}
+
+for (i = 9; i < 10; i++) {
+  document.write('bbbbbbbbbb');
+  document.write('<br>');
+}
